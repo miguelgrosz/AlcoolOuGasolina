d258ecc4da0d09d53e16d8af0ea39ba1198b29ca
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-30 20:32:49 -0300

    Tive muita dificudade nessa tarefa ;-;

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿ decimal gasolina, etanol;
+
+        Console.WriteLine("|----Qual compensa mais----|\n");
+        Console.Write("Preço da Gasolina--------(R$): ");
+        gasolina = Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("Preço da Etanol----------(R$): ");
+        etanol = Convert.ToDecimal(Console.ReadLine());
+
+        string recomendacao;
+
+        if (EncherDeGasolina(etanol, gasolina))
+        {
+            recomendacao = "Gasolina";
+        }
+        else
+        {
+            recomendacao = "Etanol";
+        }
+
+
+        double razao = CalculoDaRazao(etanol, gasolina) * 100;
+
+        Console.WriteLine($"\nO Etanol custa {razao:N1}% do preço da Gasolina");
+        Console.WriteLine($"\nÉ melhor abastecer com {recomendacao.ToUpper()}");
+    
+
+    static double CalculoDaRazao(decimal precoEtanol, decimal precoGasolina)
+    {
+        return Convert.ToDouble(precoEtanol / precoGasolina);
+    }
+
+    static bool EncherDeGasolina(decimal precoEtanol, decimal precoGasolina)
+    {
+        const double MinRazaoUsoGasolina = 0.73;
+        return CalculoDaRazao(precoEtanol, precoGasolina) >= MinRazaoUsoGasolina;
+    }
